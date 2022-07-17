e481fde8c3ec5bc9cf4dc0c09e563562b9d1af56
Vincent Genest <58990690+deltxprt@users.noreply.github.com>
2022-07-17 20:09:09 +0000

    set sql primary key + remove testing

--- MAPBot.py
+++ MAPBot.py
@@ -61,6 +61,8 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 if checkTableExists(mydb, "InstanceStatus") == False:
+    mycursor.execute("SET @ORIG_SQL_REQUIRE_PRIMARY_KEY = @@SQL_REQUIRE_PRIMARY_KEY")
+    mycursor.execute("SET SQL_REQUIRE_PRIMARY_KEY = 0")
     mycursor.execute("CREATE TABLE InstanceStatus (FriendlyName VARCHAR(255), ActiveUsers VARCHAR(255), MaxUsers VARCHAR(255), Game VARCHAR(255), Running VARCHAR(255), CPUUsage VARCHAR(255), MemoryUsage VARCHAR(255))")
     mydb.commit()
 
