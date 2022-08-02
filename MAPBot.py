6693cdb4c49c29895a2996995091853b58ff6fb7
Vincent Genest <58990690+deltxprt@users.noreply.github.com>
2022-08-02 17:39:22 -0400

    dropdown menu test

--- MAPBot.py
+++ MAPBot.py
@@ -28,6 +28,37 @@ CurrentTime_Format = CurrentTime.strftime("%d/%m/%Y %H:%M:%S")
 
 
 # Loading functions
+
+class Dropdown(discord.ui.Select):
+    def __init__(self):
+
+        # Set the options that will be presented inside the dropdown
+        options = [
+            discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
+            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
+            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦')
+        ]
+
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's 
+        # selected options. We only want the first one.
+        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
+
+
+class DropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown())
+
 def AMPStatus(url, do_token):
     header = {
         "accept": "application/json",
@@ -180,15 +211,14 @@ async def GetServerStatus(ctx, name):
         
         
 
-@client.command()
-async def manage(self, ctx):
-    servers = Select(options=[
-        discord.SelectOption(label = "test1", description = "This is a test"),
-        discord.SelectOption(label = "test2", description = "This is another test"),
-    ])
-    view = View()
-    view.add_item(servers)
-    await ctx.send("Choose the server", view=view)
+async def colour(ctx):
+    """Sends a message with our dropdown containing colours"""
+
+    # Create the view containing our dropdown
+    view = DropdownView()
+
+    # Sending a message containing our view
+    await ctx.send('Pick your favourite colour:', view=view)
 
 @client.event
 async def on_command_error(ctx, error):
